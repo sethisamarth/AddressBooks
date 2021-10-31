5b2434c5dc68e8cd919a0f5d34d8850e93483583
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-31 21:17:54 +0530

    Removing data

--- AddresBooks/AddresBooks/Program.cs
+++ AddresBooks/AddresBooks/Program.cs
@@ -6,10 +6,41 @@ namespace AddressBooks
     {
         static void Main(string[] args)
         {
+
+
             Console.WriteLine("Welcome To Address Book System!");
-            AddrBook.GetCustomer();
-            AddressBooks.AddrBook.Modify();
-            AddrBook.ListingPeople();
+            Console.WriteLine("1.Enter to add the details");
+            Console.WriteLine("2.Enter to modify the details");
+            Console.WriteLine("3.Listing the details..");
+            Console.WriteLine("4.Remove the details");
+            Console.WriteLine("Enter a option");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    AddrBook.GetCustomer();
+                    AddrBook.ListingPeople();
+                    break;
+                case "2":
+                    AddrBook.GetCustomer();
+                    AddrBook.Modify();
+                    AddrBook.ListingPeople();
+                    break;
+                case "3":
+                    AddrBook.GetCustomer();
+                    AddrBook.ListingPeople();
+                    break;
+                case "4":
+                    AddrBook.GetCustomer();
+                    AddrBook.RemovePeople();
+
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
+
+            }
+
+
         }
     }
 }
